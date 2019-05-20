c5488fe11d275e4b27b58d060a8d9f46a616838d
communal-principle-3181 <aellis19@outlook.com>
2019-05-20 13:16:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function currentLine(katzDeliLine)
   {
     for(var i = 0; i < katzDeliLine.length; i++)
     {
-      currentCustomer +=`${(i+1)}. ${katzDeliLine[i]},`;
+      currentCustomer +=` ${(i+1)}. ${katzDeliLine[i]},`;
     }
     return currentCustomer.slice(0,-1);
   }
