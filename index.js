97e7f3ef3db9f659f4a15a737079ceae535e2beb
communal-principle-3181 <aellis19@outlook.com>
2019-05-20 12:25:27 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,31 @@
+function takeANumber(line, customer)
+{
+  var welcomeMessage = `Welcome, ${customer}. You are number ${line.length} in line.`;
+  return welcomeMessage;
+}
+
+function nowServing(katzDeliLine)
+{
+  var nowServingMessage;
+  
+  var currentCustomer = (katzDeliLine[0]);
+  if(katzDeliLine === undefined || katzDeliLine === 0)
+  {
+    nowServingMessage = "There is nobody waiting to be served!";
+    return nowServingMessage;
+  }
+  else
+  {
+    nowServingMessage = `Currently Serving ${currentCustomer}.`;
+    katzDeliLine = katzDeliLine.slice[1];
+    return nowServingMessage;
+    
+  }
+}
+
+function currentLine(katzDeliLine)
+{
+  var customers = `The line is currently';
+  
+  if()
+}
